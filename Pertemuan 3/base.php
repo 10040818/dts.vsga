7f6f676775a9c0ce5c723f37ae8d2c9346cf0031
kumpultugas17 <kumpultugas17@gmail.com>
2020-09-30 16:30:16 +0800

    Pertemuan 3

---
+++ Pertemuan 3/base.php
@@ -0,0 +1,27 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+  <!-- link untuk loading bootstrap.css -->
+  <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+  <title>Contoh Bootstrap</title>
+</head>
+
+<body>
+  <div class="container">
+    <div class="row">
+      <div class="col-md-12">
+        <h1>Hello World</h1>
+      </div>
+    </div>
+  </div>
+
+  <!-- script untuk loading jquery.min.js  -->
+  <script src="assets/js/jquery.min.js"></script>
+  <script src="assets/js/bootstrap.min.js"></script>
+</body>
+
+</html>
