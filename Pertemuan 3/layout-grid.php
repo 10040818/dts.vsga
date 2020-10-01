961ec72122e14ccb4eec1b3abcf27b79d8722157
kumpultugas17 <kumpultugas17@gmail.com>
2020-10-01 09:11:40 +0800

    Create layout-grid.php

---
+++ Pertemuan 3/layout-grid.php
@@ -0,0 +1,58 @@
+<!Doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <!-- link untuk loading bootstrap.css -->
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+    <title>Contoh Bootstrap</title>
+</head>
+
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12 text-center">
+                <h1>DIGITAL TALENT | Grid Layout Bootstrap</h1>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col-md-12 text-center">
+                <div class="alert alert-danger">
+                    <div class="panel-body">
+                        Isi Header
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col-md-3">
+                <div class="alert alert-success">
+                    Sidebar
+                </div>
+            </div>
+            <div class="col-md-9">
+                <div class="alert alert-success">
+                    Konten
+                </div>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col-md-12 text-center">
+                <div class="alert alert-danger">
+                    <div class="panel-body">
+                        Footer
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <!-- script untuk loading jquery.min.js  -->
+        <script src="assets/js/jquery.min.js"></script>
+        <script src="assets/js/bootstrap.min.js"></script>
+</body>
+
+</html>
