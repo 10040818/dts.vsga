dbee7608fca8dcc0761cd39d23d4cdd76d1e52e3
kumpultugas17 <kumpultugas17@gmail.com>
2020-10-01 09:42:11 +0800

    Update Tugas Menu.php

---
+++ Pertemuan 3/Tugas/Tugas Menu.php
@@ -0,0 +1,121 @@
+<!Doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <!-- link untuk loading bootstrap.css -->
+    <link rel="stylesheet" href="../assets/css/bootstrap.min.css">
+    <title>Tugas 3</title>
+</head>
+
+<body>
+    <div class="container">
+        <h5>Muhammad Iqbal Adenan - JWDb - Tugas 3</h5>
+        <div class="row">
+            <div class="col-md-12">
+                <nav class="navbar navbar-default">
+                    <div class="container-fluid">
+                        <!-- Brand and toggle get grouped for better mobile display -->
+                        <div class="navbar-header">
+                            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse"
+                                data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                                <span class="sr-only">Toggle navigation</span>
+                                <span class="icon-bar"></span>
+                                <span class="icon-bar"></span>
+                                <span class="icon-bar"></span>
+                            </button>
+                            <a class="navbar-brand" href="#">DTS2020</a>
+                        </div>
+
+                        <!-- Collect the nav home, forms, and other content for toggling -->
+                        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                            <ul class="nav navbar-nav">
+                                <li class="active"><a href="#">Beranda <span class="sr-only">(current)</span></a></li>
+                                <li class="dropdown">
+                                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button"
+                                        aria-haspopup="true" aria-expanded="false">Skema Pelatihan <span
+                                            class="caret"></span>
+                                    </a>
+                                    <ul class="dropdown-menu">
+                                        <li><a href="#">Junior Web Programming</a></li>
+                                        <li><a href="#">Mobile Programming</a></li>
+                                        <li role="separator" class="divider"></li>
+                                        <li><a href="#">Network Administrator</a></li>
+                                        <li><a href="#">Drive Tester</a></li>
+                                        <li role="separator" class="divider"></li>
+                                        <li><a href="#">Multimedia Designer</a></li>
+                                    </ul>
+                                </li>
+                                <li><a href="#">Contact Us</a></li>
+                            </ul>
+                        </div><!-- /.navbar-collapse -->
+                    </div><!-- /.container-fluid -->
+                </nav>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col-md-3 text-center">
+                <div class="alert alert-danger">
+                    Sidebar 3 Kolom
+                </div>
+            </div>
+            <div class="col-md-6 text-center">
+                <div class="alert alert-danger">
+                    Konten 6 Kolom
+                    <br><br>
+                    <div class="row">
+                        <div class="col-md-6">
+                            <div class="alert alert-warning">Sub Konten 6 Kolom</div>
+                        </div>
+                        <div class="col-md-6">
+                            <div class="alert alert-warning">Sub Konten 6 Kolom</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-3 text-left">
+                <div class="alert alert-success">
+                    Sidebar 3 Kolom
+                    <br><br>
+                    <form action="">
+                        <div class="form-group">
+                            <label for="usr">Username</label>
+                            <input type="text" class="form-control" id="usr" placeholder="Username">
+                        </div>
+                        <div class="form-group">
+                            <label for="psw">Password</label>
+                            <input type="password" class="form-control" id="psw" placeholder="Password">
+                        </div>
+                        <button type="submit" class="btn btn-primary">Login</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col-md-4 text-center">
+                <div class="alert alert-info">
+                    Footer 4 Kolom
+                </div>
+            </div>
+            <div class="col-md-4 text-center">
+                <div class="alert alert-info">
+                    Footer 4 Kolom
+                </div>
+            </div>
+            <div class="col-md-4 text-center">
+                <div class="alert alert-info">
+                    Footer 4 Kolom
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- script untuk loading jquery.min.js  -->
+    <script src="../assets/js/jquery.min.js"></script>
+    <script src="../assets/js/bootstrap.min.js"></script>
+</body>
+
+</html>
