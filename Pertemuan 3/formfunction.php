53913172af75cb30b0028a7460afdfe06e00b4b8
kumpultugas17 <kumpultugas17@gmail.com>
2020-10-03 17:59:07 +0800

    Insert Images & Screenshoot

---
+++ Pertemuan 3/formfunction.php
@@ -0,0 +1,56 @@
+<!Doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <!-- link untuk loading bootstrap.css -->
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+    <title>Contoh Form</title>
+</head>
+
+<?php
+function input_text($label)
+{
+?>
+<div class="form-group">
+    <label for="nm">Nama Anda</label>
+    <input type="text" class="form-control" id="nm" placeholder="Nama Anda">
+</div>
+<?php
+}
+?>
+
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <h1>Digital Talent: Form Bootstrap</h1>
+                <form action="">
+                    <div class="form-group">
+                        <label for="nm">Nama Anda</label>
+                        <input type="text" class="form-control" id="nm" placeholder="Nama Anda">
+                    </div>
+                    <div class="form-group">
+                        <label for="alamat">Alamat</label>
+                        <textarea type="text" class="form-control" id="alamat" placeholder="Alamat"></textarea>
+                    </div>
+                    <div class="form-group">
+                        <label for="jk">Jenis Kelamin</label>
+                        <select class="form-control">
+                            <option>Laki-laki</option>
+                            <option>Perempuan</option>
+                        </select>
+                    </div>
+                    <button type="submit" class="btn btn-primary">Simpan</button>
+                </form>
+            </div>
+        </div>
+    </div>
+
+    <!-- script untuk loading jquery.min.js  -->
+    <script src="assets/js/jquery.min.js"></script>
+    <script src="assets/js/bootstrap.min.js"></script>
+</body>
+
+</html>
