61eb5b0d34cb437126fcf6aa805ea81fdcef6d8f
kumpultugas17 <kumpultugas17@gmail.com>
2020-10-07 16:05:32 +0800

    Update foreach.php

--- Pertemuan 10/foreach.php
+++ Pertemuan 10/foreach.php
@@ -1,11 +1,27 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-	<meta charset="UTF-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>Contoh Foreach</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Contoh Foreach</title>
 </head>
+
 <body>
-	<?php $nama = array('Adi','Budi', 'Carli', 'Dedy'); ?>
+    <?php $nama = array('Adi', 'Budi', 'Carli', 'Dedy'); ?>
+    <table>
+        <tr>
+            <td>No</td>
+            <td>Nama</td>
+        </tr>
+        <?php $no = 1; ?>
+        <?php foreach ($nama as $row) { ?>
+        <tr>
+            <td><?= $no; ?></td>
+            <td><?= $val ?></td>
+        </tr>
+        <?php } ?>
+    </table>
 </body>
+
 </html>
